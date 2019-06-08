fa634818c803f34abdd9fc1a1b9f47180264fe5b
John Conley <johnconley507@hotmail.com>
2019-06-08 13:57:36 -0400

    setting up datastore and querying for existiexisting entities

--- data-functions/index.js
+++ data-functions/index.js
@@ -16,3 +16,23 @@ exports.imageTagger = (event, callback) => {
 		return;
 	}
 }	
+
+function processLabels(bucketObject) {
+	const storagePath = 'gs://${bucketObject.bucket}/${bucketObject.name}';
+	const query = datastore.createQuery('images').select('__key__').limit(1);
+	query.filter('storagePath', '=', storagePath);
+
+	return query.run().then(data => {
+		const objectExists = data[0].length > 0;
+		const key = objectExists ? data[0][0][datastore.KEY] : datastore.key['Images'];
+
+		if(objectExists && bucketObject.resource == 'not_exists') {
+			return datastore.delete(key).then(() => {
+				console.log('Successfully deleted entity.');
+			});
+		}
+	})
+	.catch(err => {
+		console.log('Query run received an error', err);
+	})
+}
